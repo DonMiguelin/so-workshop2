f18d85cdef9ef430b9eaa3f66c461f90419eb2ac
santyfajardo <32053327+santyfajardo@users.noreply.github.com>
2018-03-14 07:36:22 -0500

    Submit workshop2 (#6)
    * Submit workshop2

---
+++ A00328044/syscall_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+printf("Hola \n");
+struct stat structa ={0};
+if(stat("/tmp/so-workshop2/A00328044/SistemasOperativos", &structa)==-1){
+printf("No esta \n");
+}else{
+printf("Esta \n");
+}
+
+}
+
